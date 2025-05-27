13629b4ba46577dd8cd6a243efc4da407bcfd5db
djid-dev <116326083+djid-dev@users.noreply.github.com>
2025-05-26 21:01:19 -0400

    fix(api): update date endpoint to handle valid and invalid dates

--- index.js
+++ index.js
@@ -19,9 +19,21 @@ app.get("/", function (req, res) {
 });
 
 
+
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+
+  const date = dateParam ? new Date(dateParam) : new Date();
+  if (isNaN(date.getTime())) {
+    // If the date is invalid, return an error
+    return res.json({ error: "Invalid Date" });
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
